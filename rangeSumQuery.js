5bfb0f5e81ab640bf3da31e65c4d3bb67be78342
RobertBowie <robertb1903@gmail.com>
2016-03-19 14:07:53 -0700

    Naive attempt to add memoization mechanism.

--- rangeSumQuery.js
+++ rangeSumQuery.js
@@ -14,15 +14,22 @@ There are many calls to sumRange function.
 
 function sumRange(start, end) {
   var sum = 0;
+  // Optimizations for previously computed ranges:
+  var prevResults = prevResults || {};
+  var id = start + ',' + end;
+  if (prevResults[id]) { console.log('previously computed value: ');  return prevResults[id] };
   for (var i = start; i <= end; i++) {
     sum += nums[i];
   }
+  prevResults[id] = sum;
+  console.log(prevResults);
   return sum;
 };
 
 // Tests:
 nums = [-2, 0, 3, -5, 2, -1];
 
+console.log(sumRange(0, 2)); // -> 1
 console.log(sumRange(0, 2)); // -> 1
 console.log(sumRange(2, 5)); // -> -1
 console.log(sumRange(0, 5)); // -> -3
