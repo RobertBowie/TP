85db9e66833e2a38f9f8b17f42f76a41011b8ef4
RobertBowie <robertb1903@gmail.com>
2016-03-19 13:31:14 -0700

    Adds working naive solution.

--- rangeSumQuery.js
+++ rangeSumQuery.js
@@ -11,3 +11,18 @@ Note:
 You may assume that the array does not change.
 There are many calls to sumRange function.
 */
+
+function sumRange(start, end) {
+  var sum = 0;
+  for (var i = start; i <= end; i++) {
+    sum += nums[i];
+  }
+  return sum;
+};
+
+// Tests:
+nums = [-2, 0, 3, -5, 2, -1];
+
+console.log(sumRange(0, 2)); // -> 1
+console.log(sumRange(2, 5)); // -> -1
+console.log(sumRange(0, 5)); // -> -3
