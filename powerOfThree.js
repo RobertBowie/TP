7841b36684141106357ef4c043b40d14fbd3ec28
RobertBowie <robertb1903@gmail.com>
2016-03-22 21:25:39 -0700

    Adds basic solution.

--- powerOfThree.js
+++ powerOfThree.js
@@ -3,5 +3,16 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-  
+  var temp = n;
+  while (temp > 1) {
+    temp = temp / 3;
+  }
+  return temp % 1 === 0;
 };
+
+console.log(isPowerOfThree(3));
+console.log(isPowerOfThree(6));
+console.log(isPowerOfThree(9));
+console.log(isPowerOfThree(27));
+console.log(isPowerOfThree(28));
+console.log(isPowerOfThree(81));
