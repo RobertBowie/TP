9ff5343c2bfc0051f9c9a4461fee9b47c780d0be
RobertBowie <robertb1903@gmail.com>
2016-03-25 16:59:06 -0700

    Adds solution.

--- nimTp.js
+++ nimTp.js
@@ -3,5 +3,14 @@
  * @return {boolean}
  */
 var canWinNim = function(n) {
-  
+  var canWin = false;
+  // We can remove 1-3 pieces on our turn
+  for (var i = 1; i <= 3; i++) {
+    // if n % 4 === 0 
+    if ((n - i) % 4 === 0) {
+      // we could win
+      canWin = true;
+    }
+  }
+  return canWin;
 };
