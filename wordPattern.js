049e262d0c6ee0c49e523cddc5e1d5c86cc75471
RobertBowie <robertb1903@gmail.com>
2016-04-15 13:48:11 -0700

    Adds solution and basic tests.

--- wordPattern.js
+++ wordPattern.js
@@ -18,5 +18,41 @@ You may assume pattern contains only lowercase letters, and str contains lowerca
  * @return {boolean}
  */
 var wordPattern = function(pattern, str) {
-  
+  var strArr = str.split(' ');
+  var pattLen = pattern.length;
+  var associations = {};
+  // The most basic check: compare the length of pattern to the length of str
+  if (pattLen !== strArr.length) { return false; }
+  // Iterate over pattern and build an object that holds associations
+  for (var i = 0; i < pattLen; i++) {
+    var pattChar = pattern[i];
+    var strWord = strArr[i];
+    if (pattChar in associations) {
+      if (associations[pattChar] !== strWord) { return false; }
+    } else {
+      // make sure the values in associations are unique
+      for (var key in associations) {
+        if (associations[key] === strWord) { return false; }
+      }
+      associations[pattChar] = strWord;
+    }
+  }
+  return true;
 };
+
+
+var pattern, str;
+pattern = "abba", str = "dog cat cat dog";
+console.log(wordPattern(pattern, str));
+
+pattern = "abba", str = "dog cat cat fish";
+console.log(wordPattern(pattern, str));
+
+pattern = "aaaa", str = "dog cat cat dog";
+console.log(wordPattern(pattern, str));
+
+pattern = "abba", str = "dog dog dog dog";
+console.log(wordPattern(pattern, str));
+
+pattern = "abb", str = "dog dog dog dog";
+console.log(wordPattern(pattern, str));
