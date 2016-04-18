91a67ceb7d188a273ecef477349235775b9c281d
RobertBowie <robertb1903@gmail.com>
2016-04-18 16:47:24 -0700

    Adds solution.

--- btRsv.js
+++ btRsv.js
@@ -23,5 +23,39 @@ You should return [1, 3, 4]
  * @return {number[]}
  */
 var rightSideView = function(root) {
-  
+  var results = [];
+  var depth = 1;
+  var maxDepth = 0;
+  function treeWalker(node, depth) {
+    if (node === undefined) { return; }
+    if (maxDepth < depth) {
+      results.push(node.val);
+      maxDepth = depth;
+    }
+    if (node.right) {
+      treeWalker(node.right, depth + 1);
+    }
+    if (node.left) {
+      treeWalker(node.left, depth + 1);
+    }
+    
+  }
+  treeWalker(root, depth);
+  return results;
 };
+
+
+// Test it
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+var testTree = new TreeNode(1);
+testTree.right = new TreeNode(3);
+testTree.left = new TreeNode(2);
+testTree.left.right = new TreeNode(5);
+testTree.right.right = new TreeNode(4);
+
+console.log(rightSideView(testTree));
