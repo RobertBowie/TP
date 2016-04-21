a561893458c9a75c67c8c64c81af85b64be40d45
RobertBowie <robertb1903@gmail.com>
2016-04-21 15:47:12 -0700

    Adds prompt.

---
+++ sortedListToBST.js
@@ -0,0 +1,25 @@
+/*
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+  
+};
