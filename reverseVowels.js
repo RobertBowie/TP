0100da15fbb9c0629c0371db511f5233f476a7f3
RobertBowie <robertb1903@gmail.com>
2016-04-27 12:33:34 -0700

    Adds prompt.

---
+++ reverseVowels.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+  
+};
