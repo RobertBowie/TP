d265667f3d60bb34f979ac63024ab0785a64fe12
RobertBowie <robertb1903@gmail.com>
2016-04-28 13:49:10 -0700

    Adds prompt.

---
+++ spiralMatrixTraversal.js
@@ -0,0 +1,14 @@
+// Write a function to print a matrix in a spiral
+// For example, if the matrix is
+//  1  2  3  4
+//  5  6  7  8
+//  9 10 11 12
+// 13 14 15 16
+// the output might be
+// 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+// [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+// [[1, 2, 3, 4]]
+// [[1, 2], [3, 4, 5, 6], [7, 8, 9, 10, 11, 12]]
+function printSpiralMatrix(matrix) {
+  
+};
