3b7fb65eb9fdb03ccb568044858177731d018d16
RobertBowie <robertb1903@gmail.com>
2016-05-07 15:49:04 -0700

    Adds prompt.

---
+++ houseRobberIII.js
@@ -0,0 +1,40 @@
+/*
+The thief has found himself a new place for his thievery again.
+There is only one entrance to this area, called the "root".
+Besides the root, each house has one and only one parent house.
+After a tour, the smart thief realized that "these houses form
+a binary tree". The police will be contacted if two directly-linked
+houses are broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var rob = function(root) {
+  
+};
