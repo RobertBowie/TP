d8c59b8020ea11b7c83357ccab56c2582a2a7a5b
RobertBowie <robertb1903@gmail.com>
2016-05-07 23:24:58 -0700

    Adds solution.

--- houseRobberIII.js
+++ houseRobberIII.js
@@ -36,5 +36,57 @@ Maximum amount of money the thief can rob = 4 + 5 = 9.
  * @return {number}
  */
 var rob = function(root) {
-  
+  // if root === null return 0
+  if (root === null) { return 0; }
+  var currVal = 0;
+  // if (left child)
+  if (root.left) {
+    // add sum of robbed children of root.left to currVal
+    currVal += rob(root.left.left) + rob(root.left.right);
+  }
+  // if (right child)
+  if (root.right) {
+    // add sum of robbed children of root.right to currVal
+    currVal += rob(root.right.left) + rob(root.right.right);
+  }
+  // return the greater of root.val + currVal and the sum of robbing roots children
+  return Math.max(root.val + currVal, rob(root.right) + rob(root.left));
 };
+
+// Test:
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+};
+
+/*
+     3
+    / \
+   2   3
+    \   \
+     3   1
+*/
+
+var test1 = new TreeNode(3);
+test1.right = new TreeNode(3);
+test1.right.right = new TreeNode(1);
+test1.left = new TreeNode(2);
+test1.left.right = new TreeNode(3);
+
+console.log(rob(test1)); // 7
+
+/*
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+*/
+var test2 = new TreeNode(3);
+test2.left = new TreeNode(4);
+test2.left.left = new TreeNode(1);
+test2.left.right = new TreeNode(3);
+test2.right = new TreeNode(5);
+test2.right.right = new TreeNode(1);
+
+console.log(rob(test2)); // 9
