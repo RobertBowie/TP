48c877361e13eb29bf7b05d601c8df265b2d8d78
RobertBowie <robertb1903@gmail.com>
2016-05-09 08:17:06 -0700

    Adds prompt.

---
+++ phonePadMap.js
@@ -0,0 +1,14 @@
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+*/
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  
+};
