d70d8b72186739e13cf26335a440dc849bf074e0
RobertBowie <robertb1903@gmail.com>
2016-05-10 13:02:05 -0700

    Adds a test.

--- mergeSortedLists.js
+++ mergeSortedLists.js
@@ -17,3 +17,20 @@ Merge two sorted linked lists and return it as a new list. The new list should b
 var mergeTwoLists = function(l1, l2) {
   
 };
+
+// Test:
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+};
+
+var list1 = new ListNode(1);
+list1.next = new ListNode(3);
+list1.next.next = new ListNode(5);
+
+var list2 = new ListNode(2);
+list2.next = new ListNode(4);
+list2.next.next = ListNode(6);
+
+console.log(mergeTwoLists(list1, list2)); // 1 -> 2 -> 3 -> 4 -> 5 -> 6
