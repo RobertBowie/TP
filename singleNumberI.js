589c01a332bee41055c2e7964892e636c5ba20c0
RobertBowie <robertb1903@gmail.com>
2016-05-11 08:29:02 -0700

    Adds basic tests.

--- singleNumberI.js
+++ singleNumberI.js
@@ -12,3 +12,15 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 var singleNumber = function(nums) {
   
 };
+
+// Test:
+
+var test1 = [1, 1, 2, 3, 3];
+var test2 = [1, 2, 3, 2, 1];
+var test3 = [10, 20, 20, 10, 5, 7, 8, 5, 8];
+var test4 = [2, 3, 2];
+
+console.log(singleNumber(test1)); // 2
+console.log(singleNumber(test2)); // 3
+console.log(singleNumber(test3)); // 7
+console.log(singleNumber(test4)); // 3
