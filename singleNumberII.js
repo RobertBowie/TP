4cfb647a8d6ea1859b347df8a84b2c30755ae35b
RobertBowie <robertb1903@gmail.com>
2016-05-12 13:16:04 -0700

    Adds pseudo code.

--- singleNumberII.js
+++ singleNumberII.js
@@ -10,5 +10,18 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
  * @return {number}
  */
 var singleNumber = function(nums) {
-  
+  // sort nums
+
+  // shift off sortedNums into a var num
+
+  // while sortedNums.length
+
+    // if num !== sortedNums[0]
+
+      // return num
+
+    // else
+
+      // shift 3 times, storing in num
+
 };
