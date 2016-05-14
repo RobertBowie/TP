1ec7dd994ae5981b5e13ece34cb66eafa69c1891
RobertBowie <robertb1903@gmail.com>
2016-05-14 15:25:25 -0700

    Adds naive solution.

--- bulbSwitcher.js
+++ bulbSwitcher.js
@@ -22,5 +22,27 @@ bulb. Find how many bulbs are on after n rounds.
  * @return {number}
  */
 var bulbSwitch = function(n) {
-  
+  // build an array containing bulb state
+  var bulbStates = [];
+  for (var i = 0; i < n; i++) {
+    bulbStates.push(false);
+  }
+  // for n rounds
+  for (var j = 1; j <= n; j++) {
+    // toggle jth bulbs
+    for (var k = j - 1; k < n; k += j) {
+      bulbStates[k] = !bulbStates[k];
+    }
+  }
+
+  // return the count of true bulbs
+  return bulbStates.reduce(function(prev, curr) {
+    return curr ? prev + 1 : prev;
+  }, 0);
+
 };
+
+console.log(bulbSwitch(3)); // 1
+console.log(bulbSwitch(4)); // 2
+console.log(bulbSwitch(50)); // 7
+console.log(bulbSwitch(9999999)); // 3162
