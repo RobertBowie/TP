08a1cc28738c7ef0427a83d87ad17540e46e7aeb
RobertBowie <robertb1903@gmail.com>
2016-05-16 13:34:46 -0700

    Adds prompt.

---
+++ maxDepthBT.js
@@ -0,0 +1,20 @@
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+*/
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  
+};
+
+// Test:
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+};
