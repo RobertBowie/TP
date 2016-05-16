7439761aa0d7a46ce687ae0743853cead0713862
RobertBowie <robertb1903@gmail.com>
2016-05-15 22:15:07 -0700

    Changes prompt further.  Adds another solution.

--- rotateArray.js
+++ rotateArray.js
@@ -1,5 +1,5 @@
 /*
-Rotate an array of n elements to the right by k steps.
+Rotate an array of n elements to the left by k steps.
 
 For example, with n = 7 and k = 4, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
 
@@ -15,13 +15,17 @@ Try to come up as many solutions as you can, there are at least 3 different ways
 var rotate = function(nums, k) {
   // handle k larger than nums.length
   if (k > nums.length) { k %= nums.length; }
-  // return the first k elements of nums appended to the end
-  return nums.concat(nums.splice(0, k));
+  // shift() and push() k elements
+  for (k; k > 0; k--) {
+    nums.push(nums.shift());
+  }
 };
 
 // Test:
 var test1 = [1, 2, 3, 4, 5, 6, 7];
-console.log(rotate(test1, 4)); // [5,6,7,1,2,3,4]
+rotate(test1, 4);
+console.log(test1); // [5, 6, 7, 1, 2, 3, 4]
 
 var test2 = [1, 2, 3];
-console.log(rotate(test2, 7));
+rotate(test2, 7);
+console.log(test2); // [2, 3, 1]
