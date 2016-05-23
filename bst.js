de757e7e4dd22b40657b76b32ddc97e7f970f725
RobertBowie <robertb1903@gmail.com>
2016-05-23 14:24:51 -0700

    Adds and tests most of .delete().

--- bst.js
+++ bst.js
@@ -80,30 +80,60 @@ BST.prototype = {
       }
     }
   },
-  delete: function (val, node) {
+  delete: function (val, node, parent) {
     node = node || this.root;
+    var currVal = node.val;
     if (node === null) { return; }
-    if (node.val === val) {
-      // handle deletion
+    // if root is the only value and is to be deleted
+    if (this.root.val === val && this.root.left === null && this.root.right === null) {
+      this.root = null;
+    } else if (currVal === val) { // if node.val matches val
+      if (node.left === null && node.right === null) { // if the node to delete is a leaf
+        // set the node's link from parent to null
+        if (currVal < parent.val) { parent.left = null; }
+        if (currVal > parent.val) { parent.right = null; }
+      } else if (node.right !== null && node.left === null) { // if the node to delete has only right child
+        // set the node's link from parent to right child
+        if (currVal < parent.val) { parent.left = node.right; }
+        if (currVal > parent.val) { parent.right = node.right; }
+      } else if (node.right === null && node.left !== null) { // if the node to delete has only left child
+        // set the node's link from parent to left child
+        if (currVal < parent.val) { parent.left = node.left; }
+        if (currVal > parent.val) { parent.right = node.left; }
+      } else if (false) { // if the node has two children
+        // overwrite the currVal with the min val in the right subtree
+
+        // delete the min val
+
+      }
     } else {
-      // recurse left and right
+      // recurse left or right
+      if (val < currVal && node.left !== null) {
+        this.delete(val, node.left, node);
+      }
+      if (val > currVal && node.right !== null) {
+        this.delete(val, node.right, node);
+      }
     }
   }
 }
 
 // Test:
 var testBST = new BST;
-console.log(testBST); // {root: null}
+// console.log(testBST); // {root: null}
 testBST.insert(5);
-console.log(testBST); // {root: {val: 5, right: null, left: null}}
+// console.log(testBST); // {root: {val: 5, right: null, left: null}}
 testBST.insert(4);
-console.log(testBST); // {root: {val: 5, right: null, left: {val: 4, left: null, right: null}}}
+// console.log(testBST); // {root: {val: 5, right: null, left: {val: 4, left: null, right: null}}}
+testBST.insert(3);
 testBST.insert(6);
-console.log(testBST); // {root: {val: 5, right: {val: 6, left: null, right: null}, left: {val: 4, left: null, right: null}}}
+// console.log(testBST); // {root: {val: 5, right: {val: 6, left: null, right: null}, left: {val: 4, left: null, right: null}}}
 testBST.insert(7);
-console.log(testBST.root.right); // {val: 6, right: {val: 7, left: null, right: null}, left: null}
-console.log(testBST.lookup(7)); // true
-console.log(testBST.lookup(4)); // true
-console.log(testBST.lookup(10)); // false
-testBST.delete(7);
-console.log(testBST.lookup(7)); //false
+// console.log(testBST.root.right); // {val: 6, right: {val: 7, left: null, right: null}, left: null}
+// console.log(testBST.lookup(7)); // true
+// console.log(testBST.lookup(4)); // true
+// console.log(testBST.lookup(10)); // false
+testBST.delete(4);
+testBST.delete(6);
+console.log(testBST.lookup(4)); //false
+console.log(testBST);
