4f49da3790c2de61160ce7e6f1cd44cda6204142
RobertBowie <robertb1903@gmail.com>
2016-06-07 13:35:29 -0700

    Adds naive solution

--- validAnagram.js
+++ validAnagram.js
@@ -18,5 +18,44 @@ What if the inputs contain unicode characters? How would you adapt your solution
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  
+  // basic check: are the inputs of equal length
+  if (s.length !== t.length) { return false; }
+
+  // populate an object with the contents of s
+  var letterFreq = {};
+  s.split('').forEach(function(letter) {
+    // if the letter isn't in letterFreq
+    if (!(letter in letterFreq)) {
+      letterFreq[letter] = 1;
+    } else {
+    // otherwise increment freq
+    letterFreq[letter]++;
+    }
+  });
+
+  // for each letter in t
+  for (var i = 0; i < t.length; i++) {
+    var letter = t[i];
+    // if the letter isn't in letterFreq
+    if (!(letter in letterFreq)) {
+      // return false
+      return false;
+    }
+    // otherwise if the freq is 1
+    if (letterFreq[letter] === 1) {
+      // delete the key
+      delete letterFreq[letter];
+      continue;
+    }
+    // or decrement the value of the freq
+    letterFreq[letter]--;
+  }
+
+  // return true if letterFreq is empty else false
+  return Object.keys(letterFreq).length === 0 && letterFreq.constructor === Object;
 };
+
+// Test
+var a1 = "anagram";
+var b1 = "nagaram";
+console.log(isAnagram(a1, b1));
