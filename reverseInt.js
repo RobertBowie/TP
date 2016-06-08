ef3c732fb9aaf223e835e25803fa891d5bfa861f
RobertBowie <robertb1903@gmail.com>
2016-06-07 19:54:54 -0700

    Changes math solution, inline ternary to round -168ms

--- reverseInt.js
+++ reverseInt.js
@@ -13,18 +13,12 @@ var reverse = function(x) {
   var result = 0;
   while (x) {
     result = 10 * result + x % 10;
-    x = round(x / 10);
+    x = x > 0 ? Math.floor(x / 10) : x < 0 ? Math.ceil(x / 10) : 0;
   }
   return result;
-};
-
-function round(x) {
-  if (x === 0) { return 0; }
-  if (x > 0) { return Math.floor(x); }
-  if (x < 0) { return Math.ceil(x); }
 };
 
 console.log(reverse(123));
 console.log(reverse(-123));
 
-// Runtime on leetcode: 172ms
+// Runtime on leetcode: 168ms
