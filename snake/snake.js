3afe8713af7008bd95f387a8c98b310860cf708a
RobertBowie <robertb1903@gmail.com>
2016-06-09 15:49:39 -0700

    Fills out Snake methods

--- snake/snake.js
+++ snake/snake.js
@@ -39,56 +39,77 @@ Board.prototype = {
 // 1 x ? snake, represented by 2 in the matrix
 function Snake() {
   this.currDir = 'n';
-  this.headPos = [4, 4];
-  this.preTailPos = [5, 4];
-  this.tailPos = [6, 4];
-  // snake grows 1 every food eaten
+  this.position = [[4, 4], [5, 4], [6, 4]];
 
-  // snake dies if it hits walls (oob) or itself
+  // TODO: snake cannot double back on itself (eg. currDir cannot go from N to S)
 
-}
+};
 
 Snake.prototype = {
   constructor: Snake,
-  move: function() {
-    // extend head a space in the current direction
-    if (this.currDir === 'n') {
-      this.headPos[0] -= 1;
-    }
-    game1.setSpace(this.headPos, 2);
-    // remove tail from current space
-    game1.setSpace(this.tailPos, 0);
-    // update tailPos
-    this.tailPos[0] -= 1;
-  },
   grow: function() {
-    // like move but don't remove the tail
-
+    // find next space in current direction
+    var nextSpace = this.nextSpace();
+    // extend head a space in the current direction
+    this.position.unshift(nextSpace);
+    game1.setSpace(nextSpace, 2);
+  },
+  move: function() {
+    // like grow but remove the tail
+    this.grow();
+    game1.setSpace(this.position.pop(), 0);
   },
   die: function() {
     // what happens when the snake dies?  game over
-
+    console.warn('Game Over');
   },
   decide: function(row, col) {
     // based on next cell to encounter
-
+    var nextSpace = this.nextSpace();
     // if clear
-
+    if (nextSpace === 0) {
       // move there
-
+      this.move();
+    }
     // if snake (2) or wall (undefined)
-
+    if (nextSpace === 2 || nextSpace === null) {
       // die
-
+      this.die();
+    }
     // if food (1)
-
+    if (nextSpace === 1) {
       // grow
+      this.grow();
+    }
   },
   hatch: function() {
+    // TODO: make this dynamic based on board size
+
     // by default we have a 3-long snake in the middle of the board
     game1.setSpace([4, 4], 2);
     game1.setSpace([5, 4], 2);
     game1.setSpace([6, 4], 2);
+  },
+  nextSpace: function() {
+    // based on currDir and where the head is at return a tuple for the next space
+    var newLoc;
+    if (this.currDir === 'n') {
+      if (this.position[0][0] - 1 < 0) { return null; }
+      newLoc = [this.position[0][0] - 1, this.position[0][1]];
+    }
+    if (this.currDir === 'e') {
+      if (this.position[0][1] + 1 > board1.size - 1) { return null; }
+      newLoc = [this.position[0][0], this.position[0][1] + 1];
+    }
+    if (this.currDir === 's') {
+      if (this.position[0][0] + 1 > board1.size - 1) { return null; }
+      newLoc = [this.position[0][0] + 1, this.position[0][1]];
+    }
+    if (this.currDir === 'w') {
+      if (this.position[0][1] - 1 < 0) { return null; }
+      newLoc = [this.position[0][0], this.position[0][1] - 1];
+    }
+    return newLoc;
   }
 }
 
