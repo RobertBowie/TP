54bff551b90f94929e9088d3f21928dd77b1fc69
RobertBowie <robertb1903@gmail.com>
2016-06-19 15:08:46 -0700

    Adds solution - O(nlog(n), O(1) space

--- findDuplicateNum.js
+++ findDuplicateNum.js
@@ -6,7 +6,7 @@ is only one duplicate number, find the duplicate one.
 Note:
 You must not modify the array (assume the array is read only).
 You must use only constant, O(1) extra space.
-Your runtime complexity should be less than O(n2).
+Your runtime complexity should be less than O(n^2).
 There is only one duplicate number in the array, but it could be repeated more than once.
 */
 
@@ -15,13 +15,31 @@ There is only one duplicate number in the array, but it could be repeated more t
  * @return {number}
  */
 var findDuplicate = function(nums) {
-  var freq = {};
-  for (var i = 0; i < nums.length; i++) {
-    var curr = nums[i];
-    if (curr in freq) {
-      return curr;
+  // define our search range
+  var low = 1;
+  var high = nums.length - 1;
+  var mid, count;
+  // until we are left with one number
+  while (low < high) {
+    count = 0;
+    mid = Math.floor(low + (high - low)/2);
+    // count the nums that are <= mid
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] <= mid) {
+        count++;
+      }
+    }
+    // refine the search range
+    if (count <= mid) {
+      low = mid + 1;
     } else {
-      freq[curr] = 1;
+      high = mid;
     }
   }
+  return low;
 };
+
+let test = [1,3,2,4,5,4];
+console.log(findDuplicate(test));
+let test2 = [1,3,4,2,2];
+console.log(findDuplicate(test2));
