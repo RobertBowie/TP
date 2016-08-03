760fe261c31b356f4ca9d4f5e1944e2fb7c8d5d8
RobertBowie <robertb1903@gmail.com>
2016-08-03 14:34:30 -0700

    Adds non-nested solution and additional tests.

--- deepCompare.js
+++ deepCompare.js
@@ -1,9 +1,29 @@
 /* Write a function that compares the keys and values of two objects, returning true if they are equal */
 
 function deepCompare(ob1, ob2) {
+  var result = true;
+  forEach(ob1, (val, key, collection) => {
+    if (key in ob2) {
+      // check the val against val
+      if (val !== ob2[key]) {
+        result = false;
+      }
+    } else {
+      result = false;
+    }
+  });
+  return result;
+};
 
-}
+// Helper
+function forEach(ob, cb) {
+  for (var key in ob) {
+    cb(ob[key], key, ob);
+  }
+};
 
 // Test:
 console.log(deepCompare({name: 'Bruce'}, {name: 'Bruce'})); // True
 console.log(deepCompare({name: 'Bruce'}, {name: 'Steven'})); // False
+console.log(deepCompare({member: {name: 'Bruce', job: 'Hunter'}}, {member: {name: 'Bruce', job: 'Hunter'}})); // True
+console.log(deepCompare({member: {name: 'Bruce', job: 'Hunter'}}, {member: {name: 'Tom', job: 'Seeker'}})); // False
