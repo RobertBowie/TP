2b0fb85d7108fe92cdb4029ef6f82682a25f9038
RobertBowie <robertb1903@gmail.com>
2016-08-03 14:18:12 -0700

    Adds prompt.

---
+++ deepCompare.js
@@ -0,0 +1,9 @@
+/* Write a function that compares the keys and values of two objects, returning true if they are equal */
+
+function deepCompare(ob1, ob2) {
+
+}
+
+// Test:
+console.log(deepCompare({name: 'Bruce'}, {name: 'Bruce'})); // True
+console.log(deepCompare({name: 'Bruce'}, {name: 'Steven'})); // False
