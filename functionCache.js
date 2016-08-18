a2755d99a9fd9582444eb91e131e0e1ffa8caccc
RobertBowie <robertb1903@gmail.com>
2016-08-18 12:30:17 -0700

    Adds initial solution.

--- functionCache.js
+++ functionCache.js
@@ -6,13 +6,39 @@ on the arguments, that were applied to the function.
 
 Example:
 */
-var complexFunction = function(arg1, arg2) { /* complex calculation in here */ };
-var cachedFunction = cache(complexFunction);
+const complexFunction = function(arg1, arg2) { return arg1 + arg2 };
+const cachedFunction = cache(complexFunction);
 
-cachedFunction('foo', 'bar'); // complex function should be executed
-cachedFunction('foo', 'bar'); // complex function should not be invoked again, instead the cached result should be returned
-cachedFunction('foo', 'baz'); // should be executed, because the method wasn't invoked before with these arguments
+console.log(cachedFunction('foo', 'bar')); // complex function should be executed
+console.log(cachedFunction('foo', 'bar')); // complex function should not be invoked again, instead the cached result should be returned
+console.log(cachedFunction('foo', 'baz')); // should be executed, because the method wasn't invoked before with these arguments
+console.log(cachedFunction('foo', 'baz')); // complex function should not be invoked again, instead the cached result should be returned
+
+cachedFunction(4, 10);
+cachedFunction(4, 10);
+cachedFunction(5, 10);
+cachedFunction(5, 10);
+
 
 function cache(func) {
-  
+  // store args and return values
+  let store = {};
+
+  return function(...args) {
+    console.log('arguments to internal function are:', args);
+    // build a key from the args array
+    let key = '';
+    args.forEach( arg => key += arg + '_');
+    console.log('key happens to be: ', key);
+    // if we have called it in the past with given args
+    if (key in store) {
+      //  return the result from prev call
+      console.log('not called, returned stored result');
+      return store[key];
+    } else { // else
+      // call with given args and store result
+      console.log('called and stored');
+      return store[key] = func(...args);
+    }
+  };
 };
