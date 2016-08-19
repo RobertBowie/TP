d071fcfd11171da2f6da6634b9d7cbb42ce74c52
RobertBowie <robertb1903@gmail.com>
2016-08-19 15:23:00 -0700

    Adds solution.

--- advPigLatin.js
+++ advPigLatin.js
@@ -20,5 +20,71 @@ capitalized words.
 */
 
 function translate(sentence) {
-  
+  const vowels = {a: true, e: true, i:true, o:true, u:true};
+  const punctuation = {';': true, '.': true, ':': true, '!': true, '?': true, ',': true};
+  let words = sentence.split(' ');
+  // for each word in sentence
+  words = words.map( word => {
+    // split letters for manipulation
+    let letters = word.split('');
+    let firstChar = letters[0];
+    let lastChar = letters[letters.length - 1];
+    let punct = [];
+    // var for capital
+    let capital = firstChar === firstChar.toUpperCase();
+    if (capital) { firstChar = letters[0] = firstChar.toLowerCase(); }
+    // if there is punctuation at the end of the word
+    while (lastChar in punctuation) {
+      // store and remove punctuation
+      punct.push(lastChar);
+      letters.pop();
+      lastChar = letters[letters.length - 1];
+    }
+    // if the first letter of the word is a vowel
+    if (firstChar in vowels) {
+        // add 'way' to the end of the word
+        letters.push('w');
+        letters.push('a');
+        letters.push('y');
+        // if punctuation
+        if (punct) {
+          // append stored punctuation
+          letters = letters.concat(punct);
+        }
+        // if capital
+        if (capital) {
+          // capitalize
+          letters[0] = firstChar.toUpperCase();
+        }
+    } else { // else
+      // move consonants to end of string
+      while (!(firstChar in vowels)) {
+        letters.push(letters.shift());
+        firstChar = letters[0];
+      }
+      // add 'ay'
+      letters.push('a');
+      letters.push('y');
+      // if capital
+      if (capital) {
+        // capitalize new first letter
+        letters[0] = letters[0].toUpperCase();
+      }
+      // if punct
+      if (punct) {
+        // append punct
+        letters = letters.concat(punct);
+      }
+    }
+    // return word
+    return letters.join('');
+  });
+  // return modified sentence
+  return words.join(' ');
 };
+
+console.log(translate("Hello World")); // Ellohay Orldway
+console.log(translate("Hello World!")); // Ellohay Orldway!
+console.log(translate("Algo")); // Algoway
+console.log(translate("Pizza? Yes please!")); // "Izzapay? Esyay easeplay!"
+console.log(translate("Pizza? Yes please!!")); // "Izzapay? Esyay easeplay!!"
