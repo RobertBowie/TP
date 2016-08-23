f7cdf8732c35e61a383ba669a380e9bcf5a14886
RobertBowie <robertb1903@gmail.com>
2016-08-23 12:54:40 -0700

    Adds basics for test suite.

---
+++ test.js
@@ -0,0 +1,14 @@
+export default {
+  expect: function(expression, message) {
+    // Return either 'Test Passed' if expression is true
+
+    // or return message on false
+
+  },
+  assertEquals: function(arg1, arg2, message) {
+    // Return 'Test Passed' if arg1 and arg2 are equal
+
+    // or return message
+    
+  }
+}
