4fd184fcfaad1554cd6ba052aea458db598d1e5e
RobertBowie <robertb1903@gmail.com>
2016-08-23 13:36:52 -0700

    Adds expected output on fail.

--- test.js
+++ test.js
@@ -2,13 +2,15 @@ module.exports = {
   expect: function(expression, message) {
     if (!!expression) {
       console.log('√ - Test Passed');
+      return;
     }
     console.log('X - ' + message);
   },
   assertEquals: function(arg1, arg2, message) {
     if (arg1 == arg2) {
       console.log('√ - Test Passed');
+      return;
     }
-    console.log('X - ' + message);
+    console.log('X - ' + message, 'Expected ' + arg2 + ', got ' + arg1);
   }
 }
