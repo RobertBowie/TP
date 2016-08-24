7a8950c6a619c6db4f94c34443797f405227c236
RobertBowie <robertb1903@gmail.com>
2016-08-24 09:43:00 -0700

    Adds prompt.

---
+++ mergeSorted.js
@@ -0,0 +1,22 @@
+/*
+The name of the function in this example is "mergesorted".
+It should return a big sorted array from two smaller sorted arrays: e.g.
+
+mergesorted([1,2],[3,4]) //should: [1,2,3,4]
+
+mergesorted([1,2],[3]) //should: [1,2,3]
+
+mergesorted([1],[2, 3]) //should: [1,2,3]
+
+Since this function is being used in an algorithm, try to optimize for
+speed and browse the solutions when you're done to see how others approached
+the problem.
+*/
+
+// TODO: implement 'mergesorted'
+// assume: a and b are both arrays, consisting entirely of integers,
+// both arrays are sorted and contain at least 1 integer.
+// Do not use Array.sort
+function mergesorted(a, b) {
+  
+}
