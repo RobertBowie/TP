7e9bfd7f44cb6bf34871efcdd324ee668395a938
RobertBowie <robertb1903@gmail.com>
2016-08-24 09:53:21 -0700

    Naive solution.

--- mergeSorted.js
+++ mergeSorted.js
@@ -18,5 +18,19 @@ the problem.
 // both arrays are sorted and contain at least 1 integer.
 // Do not use Array.sort
 function mergesorted(a, b) {
-  
+  let result = [];
+  // while our lists have contents
+  while (a.length || b.length) {
+    // Take the lower of the first number in a or b
+    if (a[0] <= b[0]) {
+      // add to result array
+      result.push(a.shift());
+    } else {
+      result.push(b.shift());
+    }
+  }
+  return result;
 }
+
+// Test
+console.log(mergesorted([1, 2, 3], [1, 2, 3])); // [1, 1, 2, 2, 3, 3]
