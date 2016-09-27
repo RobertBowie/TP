e631593354b4437b3f53403417371cb825d2d0b3
RobertBowie <robertb1903@gmail.com>
2016-09-27 15:29:34 -0700

    Adds basic .decribe and .it methods to test.js

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
 module.exports = {
+  assertEquals: function(arg1, arg2, message) {
+    if (arg1 == arg2) {
+      console.log('√ - Test Passed');
+      return;
+    }
+    console.log('X - \'' + message + '\' Expected ' + arg2 + ', got ' + arg1);
+  },
+  describe: function(description, itCb) {
+    console.log(description);
+    itCb();
+  },
   expect: function(expression, message) {
     if (!!expression) {
       console.log('√ - Test Passed');
@@ -6,11 +17,8 @@ module.exports = {
     }
     console.log('X - ' + message);
   },
-  assertEquals: function(arg1, arg2, message) {
-    if (arg1 == arg2) {
-      console.log('√ - Test Passed');
-      return;
-    }
-    console.log('X - \'' + message + '\' Expected ' + arg2 + ', got ' + arg1);
+  it: function(description, cb) {
+    console.log(description);
+    cb();
   }
 }
